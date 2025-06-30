2c9cc3d590d0cdafca81649eee433fe26dc5d03f
SamDoughtyCodes <samdcodes@gmail.com>
2025-06-30 22:09:25 +0100

    Created and tested db library

---
+++ Backend/db.py
@@ -0,0 +1,26 @@
+from sqlite3 import connect
+import os
+
+# Create connection to the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, "users.db")
+conn = connect(db_path)
+cursor = conn.cursor()
+
+
+# Function to run generic SQL queries
+def query_db(query):
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
+
+# Function to compare the provided password with the stored hashed password for a user
+def check_password(username, password):
+    # Fetch the hashed password for the given username
+    query = f"SELECT hashed_password FROM users WHERE username = '{username}';"
+    result = query_db(query)[0][0]  # Array indecies needed as queries return an array
+
+    if result == password:
+        return True
+    else:
+        return False
