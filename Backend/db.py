9c4bc1dd5907ef5d5dcf56f8eb8b6fbcc4fd2ec8
SamDoughtyCodes <samdcodes@gmail.com>
2025-07-04 19:26:49 +0100

    Reduced SQL injection vulnerability

--- Backend/db.py
+++ Backend/db.py
@@ -7,20 +7,17 @@ db_path = os.path.join(script_dir, "users.db")
 conn = connect(db_path, check_same_thread=False)
 cursor = conn.cursor()
 
-
-# Function to run generic SQL queries
-def query_db(query):
-    cursor.execute(query)
-    results = cursor.fetchall()
-    return results
-
 # Function to compare the provided password with the stored hashed password for a user
 def check_password(username, password):
     # Fetch the hashed password for the given username
-    query = f"SELECT hashed_password FROM users WHERE username = '{username}';"
-    result = query_db(query)
+    query = f"SELECT hashed_password FROM users WHERE username = ?;"
+    cursor.execute(query, (username,))
+    result = cursor.fetchall()
 
     if result:
         if result[0][0] == password: return {'valid': True, 'error': None}
         else: return {'valid': False, 'error': 'Invalid Password'}
     else: return {'valid': False, 'error': 'Invalid Username'}
+
+# Function to add a new user to the database
+def add_user(username, password):
