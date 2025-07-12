ae17278b083bf4370d49e6152fca47ae12c0a0fa
SamDoughtyCodes <samdcodes@gmail.com>
2025-07-12 08:35:02 +0100

    Added event listener

--- Frontend/Login page/script.js
+++ Frontend/Login page/script.js
@@ -4,3 +4,8 @@ const { sha3_256 } = require('js-sha3');
 function hashData(data) {
     return sha3_256(data);  // Directly compute and return the hash
 }
+
+// Only run the script when the page contents have loaded
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log("Page has loaded, JS running!");
+});
