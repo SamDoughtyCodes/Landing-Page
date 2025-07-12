44950f24f3c869a666ef41d03b0b6c9a114d5d45
SamDoughtyCodes <samdcodes@gmail.com>
2025-07-12 22:10:53 +0100

    Started implimenting tokens

--- Backend/api.py
+++ Backend/api.py
@@ -1,8 +1,45 @@
+# API related imports
 from fastapi import FastAPI
 from pydantic import BaseModel
+# Self-made imports
 from db import check_password
+# Token based imports
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 app = FastAPI()
+
+# ----------- TOKENS -----------
+
+# Declare alg and secret token
+with open("SECRET_KEY.txt", "r") as f:
+    SECRET_KEY = f.readline()
+ALGORITHM = "HS256"
+security = HTTPBearer()  # Set up security method
+
+# Function to create a token, with data to encrypt, and an expiry time with a default value of 30 mins
+def create_access_token(data: dict, expire_delta: timedelta = timedelta(minutes=30)):
+    to_encode = data
+    expiry_time = datetime.now(timezone.utc) + expire_delta  # Add the expiry time to the current time to get the expiry time
+    to_encode.update({"exp": expiry_time})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)  # Create the token itself
+    return encoded_jwt
+
+# Function to verify a token
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload  #i.e. The user details
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalud or expired token"
+        )
+
+# ----------- API Endpoints -----------
 
 # Create API test call
 @app.get("/api/test")
