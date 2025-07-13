e65c21197955e16c5637047c7299786fc0cf8454
SamDoughtyCodes <samdcodes@gmail.com>
2025-07-13 08:10:58 +0100

    Fixed issue with importing the hashing library

--- Frontend/Login page/script.js
+++ Frontend/Login page/script.js
@@ -1,8 +1,11 @@
 // Import the SHA3-256 hashing function
-const { sha3_256 } = require('js-sha3');
+import sha3 from "https://cdn.jsdelivr.net/npm/js-sha3@0.9.2/+esm";
+
+const token = localStorage.getItem("token");  // Attempt to fetch the token of the user
+console.log(token);
 
 function hashData(data) {
-    return sha3_256(data);  // Directly compute and return the hash
+    return sha3.sha3_256(data);  // Directly compute and return the hash
 }
 
 // Only run the script when the page contents have loaded
