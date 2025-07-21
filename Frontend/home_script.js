d7e04d91df8d9184305e154329d3901e24eeadf1
SamDoughtyCodes <samdcodes@gmail.com>
2025-07-21 18:03:35 +0100

    Fixed issue with token (added a space to auth header)

--- Frontend/home_script.js
+++ Frontend/home_script.js
@@ -2,12 +2,9 @@ const token = localStorage.getItem("token")  // Get the token for the user (null
 
 document.addEventListener("DOMContentLoaded", (e) => {  // Only run when the content has loaded
     // --------- Use the token to verify the login ---------
-    fetch("http://localhost:8000/api/protected", {method: 'GET', headers: {'Authorization': 'Bearer' + token}})  // API call through the protected route (GET method and token in header)
+    fetch("http://localhost:8000/api/protected", {method: 'GET', headers: {'Authorization': 'Bearer ' + token}})  // API call through the protected route (GET method and token in header)
     .then(response => {  // Take the API response and run it through the following:
-        if (response.status === 403 || response.status === 401) {
-            // window.location.href = "/Login page/login.html";
-            console.log("Issue with token");
-        }  // Redirect if no token/expired token
+        if (response.status === 403 || response.status === 401) {window.location.href = "/Login page/login.html";}  // Redirect if no token/expired token
         else {return response.json();}  // Return the json message from the protected route if the token is valid
     })
     .then(data => {
